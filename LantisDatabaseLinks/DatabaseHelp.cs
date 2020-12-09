592d13bff1dddc900d83e757c80d3102783ef7ec
DESKTOP-ED070NU\Cherish <285875694@qq.com>
2020-12-09 23:23:25 +0800

    提交net core版本整理

--- LantisDatabaseLinks/DatabaseHelp.cs
+++ LantisDatabaseLinks/DatabaseHelp.cs
@@ -12,86 +12,94 @@ namespace Lantis.DatabaseLinks
 
 	public class DatabaseHelp
 	{
-
 		public static object lockSelf = new object();
-		public static SqlLinkPool sqlLinkPool;
+		public static DatabaseLinksPool sqlLinkPool;
 
 		public static void CreateSqlLinkPool(string sqlLinkStr)
 		{
-			sqlLinkPool = new SqlLinkPool();
-
-			sqlLinkPool.Instance(sqlLinkStr);
+			lock (lockSelf)
+			{
+				sqlLinkPool = new DatabaseLinksPool();
+				sqlLinkPool.Instance(sqlLinkStr);
+			}
 		}
 
 		public static int ExecuteNonQuery(string sqlStr, params DbParameter[] parameters)
 		{
-			//lock (lockSelf)
+			lock (lockSelf)
 			{
-
-				SqlLinkPool.SqlLinkState sqlLink = sqlLinkPool.SpawnInstance();
+				DatabaseLinkState sqlLink = sqlLinkPool.SpawnInstance();
 				DbConnection SqlCon = sqlLink.sqlConnectInstance;
 
 				using (DbCommand SqlCmd = SqlCon.CreateCommand())
 				{
 					SqlCmd.CommandText = sqlStr;
+
 					foreach (DbParameter parameter in parameters)
 					{
 						SqlCmd.Parameters.Add(parameter);
 
 					}
+
 					string st = SqlCmd.CommandText;
 
 					try
 					{
 						int result = SqlCmd.ExecuteNonQuery();
 						sqlLinkPool.Despawn(sqlLink);
+
 						return result;
 					}
 					catch (Exception e)
 					{
 						Logger.Error("Sql ExecuteNonQuery Exception:" + e.ToString());
 						sqlLinkPool.Despawn(sqlLink);
+
 						return 0;
 					}
-
 				}
 			}
 		}
+
 		public static object ExecuteScalar(string sqlStr, params DbParameter[] parameters)
 		{
-			//lock (lockSelf)
-			{
-				
-				SqlLinkPool.SqlLinkState sqlLink = sqlLinkPool.SpawnInstance();
+			lock (lockSelf)
+			{				
+				var sqlLink = sqlLinkPool.SpawnInstance();
 				DbConnection SqlCon = sqlLink.sqlConnectInstance;
 
 				using (DbCommand SqlCmd = SqlCon.CreateCommand())
 				{
 					SqlCmd.CommandText = sqlStr;
+
 					foreach (DbParameter parameter in parameters)
 					{
 						SqlCmd.Parameters.Add(parameter);
 					}
+
 					try
 					{
 						object result = SqlCmd.ExecuteScalar();
 						sqlLinkPool.Despawn(sqlLink);
+
 						return result;
 					}
 					catch (Exception e)
 					{
 						Logger.Error("Sql ExecuteNonQuery Exception:" + e.ToString());
 						sqlLinkPool.Despawn(sqlLink);
+
 						return null;
 					}
 				}
 			}
 		}
+
 		public static DataTable ExecuteDataTable(string sqlStr, params DbParameter[] parameters)
 		{
-			//lock (lockSelf)
+			lock (lockSelf)
 			{
-				SqlLinkPool.SqlLinkState sqlLink = sqlLinkPool.SpawnInstance();
+				var sqlLink = sqlLinkPool.SpawnInstance();
 				DbConnection SqlCon = sqlLink.sqlConnectInstance;
 
 				using (DbCommand SqlCmd = SqlCon.CreateCommand())
@@ -101,51 +109,46 @@ namespace Lantis.DatabaseLinks
 					foreach (DbParameter parameter in parameters)
 					{
 						SqlCmd.Parameters.Add(parameter);
-
 					}
 
 					DataSet dataSet = new DataSet();
-
 					DbDataAdapter dataAdapter = SqlFactorWarp.CreateAdapter(SqlCmd);
-
-					dataAdapter.Fill(dataSet);
-					
+					dataAdapter.Fill(dataSet);					
 					sqlLinkPool.Despawn(sqlLink);
 
 					if (dataSet.Tables == null)
 					{
 						return null;
 					}
+
 					if (dataSet.Tables.Count == 0)
 					{
 						return null;
 					}
 
 					return dataSet.Tables[0];
-
 				}
 			}
-
 		}
+
 		public static DbDataReader ExecuteReader(string sqlStr, params DbParameter[] parameters)
 		{
-			//lock (lockSelf)
+			lock (lockSelf)
 			{
-				SqlLinkPool.SqlLinkState sqlLink = sqlLinkPool.SpawnInstance();
+				var sqlLink = sqlLinkPool.SpawnInstance();
 				DbConnection SqlCon = sqlLink.sqlConnectInstance;
+
 				using (DbCommand SqlCmd = SqlCon.CreateCommand())
 				{
 					SqlCmd.CommandText = sqlStr;
+
 					foreach (DbParameter parameter in parameters)
 					{
 						SqlCmd.Parameters.Add(parameter);
-
 					}
 
 					sqlLinkPool.Despawn(sqlLink);
-
 					return SqlCmd.ExecuteReader();
-
 				}
 			}
 		}
---
+++ LantisDatabaseLinks/DatabaseLinksPool.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Data.Common;
+using System.Data;
+
+namespace Lantis.DatabaseLinks
+{
+	/// <summary>
+	/// 数据库连接池
+	/// </summary>
+	public class DatabaseLinksPool
+    {
+		public string linkPoolStr;
+		public int maxCount;
+		public int minCount;
+		public int curUseCount;
+		public List<DatabaseLinkState> sqlLinkPoolInstance = new List<DatabaseLinkState>();
+
+		/// <summary>
+		/// 实例化数据
+		/// </summary>
+		public void Instance(string linkStr)
+		{
+			linkPoolStr = linkStr;
+			curUseCount = 0;
+			maxCount = 60;
+			minCount = 25;
+
+			for (int i = 0; i < minCount; ++i)
+			{
+				CreateInstance(linkStr);
+			}
+
+            Logger.Log("数据持久化连接池初始化完成!");
+		}
+
+		public DatabaseLinkState CreateInstance(string linkStr)
+		{
+			DatabaseLinkState item = new DatabaseLinkState(linkStr);
+
+			item.isUsed = false;
+
+			lock (((ICollection)sqlLinkPoolInstance).SyncRoot)
+			{
+				sqlLinkPoolInstance.Add(item);
+			}
+			return item;
+		}
+
+		public DatabaseLinkState SpawnInstance()
+		{
+			CheckClear();
+
+			lock (((ICollection)sqlLinkPoolInstance).SyncRoot)
+			{
+				for (int i = sqlLinkPoolInstance.Count - 1; i >= 0; --i)
+				{
+					if (!sqlLinkPoolInstance[i].isUsed
+						&& sqlLinkPoolInstance[i].sqlConnectInstance.State != ConnectionState.Closed
+						&& sqlLinkPoolInstance[i].sqlConnectInstance.State != ConnectionState.Broken)
+					{
+						curUseCount++;
+						sqlLinkPoolInstance[i].isUsed = true;
+						return sqlLinkPoolInstance[i];
+					}
+				}
+			}
+
+			//这里没有找到
+			if (sqlLinkPoolInstance.Count < maxCount)
+			{
+				//这里可以创建
+				DatabaseLinkState curItem = CreateInstance(linkPoolStr);
+
+				curItem.isUsed = true;
+				curUseCount++;
+				return curItem;
+			}
+			else
+			{
+				System.Threading.Thread.Sleep(1);
+				DatabaseLinkState curItem = SpawnInstance();
+
+				return curItem;
+			}
+		}
+
+		public void Despawn(DatabaseLinkState desItem)
+		{
+			if (desItem.isUsed)
+			{
+				curUseCount--;
+				desItem.isUsed = false;
+
+				Console.WriteLine("当前线程池 使用中:" + curUseCount);
+			}
+		}
+
+		/// <summary>
+		/// 检测清理池
+		/// </summary>
+		public void CheckClear()
+		{
+			lock (((ICollection)sqlLinkPoolInstance).SyncRoot)
+			{
+				for (int i = sqlLinkPoolInstance.Count - 1; i >= 0; --i)
+				{
+					if (sqlLinkPoolInstance[i].sqlConnectInstance.State == ConnectionState.Closed
+						|| sqlLinkPoolInstance[i].sqlConnectInstance.State == ConnectionState.Broken)
+					{
+						sqlLinkPoolInstance[i].Dispose();
+
+						sqlLinkPoolInstance.RemoveAt(i);
+					}
+				}
+			}
+		}
+	}
+}
